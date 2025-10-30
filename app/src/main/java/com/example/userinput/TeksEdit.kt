5a2bc4eb0b36bce2ba040272d433b9151bd4f9a6
mufid1012 <mufidghibran1012@gmail.com>
2025-10-30 13:38:26 +0700

    menambahkan Horizontal Divider

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -94,10 +94,16 @@ fun FormDataDiri(modifier : Modifier
             Text(text = stringResource(id = R.string.submit))
         }
 
+        HorizontalDivider(
+            modifier = Modifier.padding(
+                bottom = dimensionResource(id = R.dimen.padding_medium),
+                top = dimensionResource(id = R.dimen.padding_medium)
+            ),
+            thickness = dimensionResource(1dp),
+            color = Color.DarkGrey
 
+        )
 
     }
-
-
 
 }
