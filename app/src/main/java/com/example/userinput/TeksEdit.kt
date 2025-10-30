22b7759923bc563e8d7f50be47ab27e9b65d5647
mufid1012 <mufidghibran1012@gmail.com>
2025-10-30 08:54:40 +0700

    menambahkan variabel-variabel untuk menyimpan untuk menyimpan data yang diperoloh dari komponen

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -1,5 +1,23 @@
 package com.example.userinput
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 
 @Composable
+fun FormDataDiri(modifier : Modifier
+){
+    //variabel-variabel untuk mengingat nilai masukkan dari keyboard
+    var textNama by remember() { mutableStateOf(value = "")}
+    var textAlamat by remember { mutableStateOf(value = "")}
+    var textJK by remember { mutableStateOf(value = "")}
+
+    //variabel-variabel untuk menyimpan untuk menyimpan data yang diperoloh dari komponen
+    var nama by remember {mutableStateOf(value = "")}
+    var alamat by remember {mutableStateOf(value = "")}
+    var jenis by remember {mutableStateOf(value = "") }
+
+
+
+}
