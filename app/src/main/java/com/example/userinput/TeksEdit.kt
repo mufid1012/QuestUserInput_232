63b8c64edee34b00848ff20ae85b4f7f0bff84ad
mufid1012 <mufidghibran1012@gmail.com>
2025-10-30 09:14:21 +0700

    menambahkan import layout row

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.Row
 
 @Composable
 fun FormDataDiri(modifier : Modifier
