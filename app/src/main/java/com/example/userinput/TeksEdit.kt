68db34772774d06f28c947a44fde67bce6e20fe4
mufid1012 <mufidghibran1012@gmail.com>
2025-10-30 09:06:12 +0700

    tambahkan field input nama lengkap pada UI

--- app/src/main/java/com/example/userinput/TeksEdit.kt
+++ app/src/main/java/com/example/userinput/TeksEdit.kt
@@ -1,8 +1,14 @@
 package com.example.userinput
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -17,6 +23,23 @@ fun FormDataDiri(modifier : Modifier
     var nama by remember {mutableStateOf(value = "")}
     var alamat by remember {mutableStateOf(value = "")}
     var jenis by remember {mutableStateOf(value = "") }
+
+    val gender: List<String> = listOf("Laki-Laki", "Perempuan")
+
+    Column(modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifer = Modifier.width(width = 250.dp),
+            label = { Text(text = "Nama Lengkap") },
+            onValueChange = {
+                textNama = it
+            }
+        )
+    }
 
 
 
