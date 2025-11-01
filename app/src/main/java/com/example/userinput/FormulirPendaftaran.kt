ade1eb326e6baef8f1f230193683b34cfbb3efcb
mufid1012 <mufidghibran1012@gmail.com>
2025-11-01 10:06:00 +0700

    menambahkan import yang belum ada

--- app/src/main/java/com/example/userinput/FormulirPendaftaran.kt
+++ app/src/main/java/com/example/userinput/FormulirPendaftaran.kt
@@ -1,14 +1,18 @@
 package com.exmaple.userinput
 
-import android.view.RoundedCorner
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Composable
